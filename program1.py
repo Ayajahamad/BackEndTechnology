9734301dec817f1fe9c7d032185c18bf67e42ffc
Ayajahamad <ayazahmad2362001@gmail.com>
2024-01-03 23:26:21 +0530

    Python First program is added

---
+++ program1.py
@@ -0,0 +1,17 @@
+# print("Hello Word")
+# print("23+3")
+
+# print("The book that help you most are those,")
+# print("     Which make you tink that most.")
+# print("         The hardest way of learning is that of easy reading;")
+# print("         But the greate book that comes from a")
+# print("     greate thinker is a ship of thought deep")
+# print("freighted with truth and beauty")
+
+print("The book that help you most are those\n "
+      "\t Which make you tink that most.\n "
+      "\t \t The hardest way of learning is that of easy reading;\n "
+      "\t \t But the greate book that comes from a\n "
+      "\t greate thinker is a ship of thought deep\n "
+      "freighted with truth and beauty")
+
