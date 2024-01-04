f35c14241993b2b343c7f811d0d93d413aed8a1d
Ayajahamad <ayazahmad2362001@gmail.com>
2024-01-04 22:32:30 +0530

    Python Printing Type is added

---
+++ program2.py
@@ -0,0 +1,14 @@
+x = int(input('Enter any Value of x :: '))
+y = float(input('Enter any Value of y :: '))
+z = (input('Enter any Value of z :: '))
+
+print(x, y, z)
+print("The value of the variables are :: ", x, y, z)
+print("the value of 'x' is ", x, "the value od 'y' is ", y, "The value of 'z' is ", z)
+print(f"the value of 'x' is{x}\n the value od 'y' is {y} \nThe value of 'z' is {z}")
+
+res = x + y
+print(f"the sum of {x} and {y} is :: ", res)
+print(type(res))
+
+
