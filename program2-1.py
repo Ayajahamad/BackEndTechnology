b0777dec11180ee35adc0702b1dd95a4daf73375
Ayajahamad <ayazahmad2362001@gmail.com>
2024-01-13 19:13:13 +0530

    Assignment Operators are added

---
+++ program2-1.py
@@ -0,0 +1,31 @@
+x = int(input("Enter Your first Number :: "))
+y = int(input("Enter Your second Number :: "))
+
+print(f"Addition of {x} and {y} is :: {x+y} \n Substraction of {x} and {y} is :: {x-y} \n Multiplication of {x} and {y} is :: {x*y} \n Division of {x} and {y} is :: {x/y} \n Modulos of {x} and {y} is :: {x%y} \n Exponent is :: {x**y} \n Floor Devision is :: {x//y}")
+
+print("Assignment Operations Are :")
+
+a = 3
+b = 5
+
+a += 4
+print(f"Add and Assign :: {a}")
+
+b = 3
+b -= 4
+print(f"Sub and Assign :: {b}")
+
+c = 3
+c *= 4
+print(f"Mul and Assign :: {c}")
+
+d = 3
+d /= 4
+print(f"Div and Assign :: {d}")
+
+a = 3
+a %= 4
+print(f"Mod and Assign :: {a}")
+
+print(4&5)
+
