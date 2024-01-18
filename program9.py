c959e234983005c210de8e71bc0b54403f85d13f
Ayajahamad <ayazahmad2362001@gmail.com>
2024-01-18 19:23:48 +0530

    Problematic solutions are added

---
+++ program9.py
@@ -0,0 +1,14 @@
+# for var_name in range(start,end)
+# for var_name in range(start,end,step_over)
+
+for x in range(1,10):
+    print(x)
+print("--------------------------------------------------------")
+for x in range(10,-1,-1):
+    print(x)
+
+
+# WAP to print a odd and even numbers between 1 to 20 and find their sum
+# WAP to find the multiples of 3 and 5 between 1 to 50 and if the number is divisible by 3 then display 3_Multiples if the number devisible by 5 then display the result as 5_multiple if both then display the result as by Both
+
+
