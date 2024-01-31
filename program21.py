784d2a1dc7d7d89aaa9c54354bc76163f2e73d73
Ayajahamad <ayazahmad2362001@gmail.com>
2024-01-31 14:55:01 +0530

    Differentiating Even and Odd Numbers and Finding there Sum

---
+++ program21.py
@@ -0,0 +1,24 @@
+numbers = (1, 2, 3, 4, 5, 6, 8, 5, 34, 23, 45, 55, 67, 87, 34, 23)
+print(f"Length :: {len(numbers)}")
+
+even_count = 0
+odd_count = 0
+
+even_sum = 0
+odd_sum = 0
+
+i = 0
+while(i < len(numbers)):
+    x = numbers[i]
+    if x % 2 == 0:
+        even_count += 1
+        even_sum += x
+    else:
+        odd_count += 1
+        odd_sum += x
+    i+=1
+
+print("Number of even numbers:", even_count)
+print("Sum of even numbers:", even_sum)
+print("Number of odd numbers:", odd_count)
+print("Sum of odd numbers:", odd_sum)
